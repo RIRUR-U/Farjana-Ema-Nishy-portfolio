cf8b569ceea1cfc2b39e4f81d1fa900bc166396e
Farjana Ema Nishy <farjananishy@gmail.com>
2023-11-10 01:26:01 +0600

    Routing & Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from '/src/components/components';
+
+
 const App = () => {
 
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar/>
+            <Hero />
+          </div>
+
+        </div> 
+     </BrowserRouter>
   )
 }
 
